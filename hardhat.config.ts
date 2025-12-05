d916a905f19dedb7d33ca63fd2fd346239df4a97
Brian-XL <523085157@qq.com>
2025-12-05 17:15:15 +0800

    add hardhat coverage

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-ethers";
 import "@nomicfoundation/hardhat-chai-matchers";
+import "solidity-coverage";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.22",
